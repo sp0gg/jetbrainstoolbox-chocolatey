87d006c28e11d31226987942c1d97c9975677256
Joe Nelson <joe.nelson@sherwin.com>
2016-11-28 14:06:17 -0500

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'jetbrainstoolbox' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.0.2095.exe' # download url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs   = '/S'           # NSIS
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'JetBrains Toolbox*'
+  checksum      = 'B560C5DAAD74C62AF9DD82496F95761C'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
