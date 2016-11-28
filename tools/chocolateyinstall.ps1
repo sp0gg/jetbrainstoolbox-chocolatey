26f8b2b8434b8213c1d0ad39e649f75acf55b35a
Joe Nelson <joe.nelson@sherwin.com>
2016-11-28 14:12:13 -0500

    Use hash from JetBrains

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -15,7 +15,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'JetBrains Toolbox*'
-  checksum      = 'B560C5DAAD74C62AF9DD82496F95761C'
+  checksum      = '567864adf507bf9b322af5565bc104372d785f2c341fd2bd5bbf3f2496e8c057'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
 }
