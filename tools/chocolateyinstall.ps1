b795d70ee90d5a155c40f56108d7a0e89f7adf19
Salome J Marquez <marquez.salomej@gmail.com>
2017-11-10 20:23:04 -0600

    Update to 1.5.2871

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'jetbrainstoolbox' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.2.2314.exe' # download url
+$url        = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.5.2871.exe' # download url
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,7 +15,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'JetBrains Toolbox*'
-  checksum      = '0b2272dce68995ca9aec290ce82b119fd26f33aec51e72310f76d64399182945'
+  checksum      = 'd56ba14f2272563e0ca4b41778d1272ef6f17a7d51f0f3ca7c8a079083207952'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
 }
